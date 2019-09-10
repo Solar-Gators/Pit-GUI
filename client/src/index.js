2d8ec3fb91a13375b1f04629c1136b358c4a3342
Raymond Bernardo <rbernardo@DESKTOP-JEDT4IF.localdomain>
2019-09-09 20:08:43 -0400

    Add backend & frontend code

---
+++ client/src/index.js
@@ -0,0 +1,9 @@
+// client/src/index.js
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+//import registerServiceWorker from './registerServiceWorker';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+//registerServiceWorker();
