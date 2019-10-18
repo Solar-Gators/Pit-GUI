71c0bdd3836f5a86419ebac741c2cc07b875c4ec
Raymond Bernardo <Seth10001@users.noreply.github.com>
2019-10-17 22:14:08 -0400

    Rename route to be more suitable

--- backend/routes/graph.server.routes.js
+++ backend/routes/graph.server.routes.js
@@ -3,7 +3,7 @@ var graph = require('../controllers/graph.server.controller.js'),
 express = require('express'), //refers to Express the middleware helper for Node.js
 router = express.Router(); //refers to the Router() function in Express the middleware helper for Node.js
 
-router.route('/info')
-.get(graph.info);
+router.route('/coord')
+.get(graph.coord);
 
 module.exports = router;
