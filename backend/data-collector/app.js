8af9c7c0a66e2546085b6df841d84854c707e1ae
Raymond Bernardo <Seth10001@users.noreply.github.com>
2019-10-22 15:26:39 -0400

    Add a data link layer

--- backend/data-collector/app.js
+++ backend/data-collector/app.js
@@ -1,16 +1,9 @@
-var SerialPort = require('serialport');
+import dataLink from './dataLink'
+var SerialPort = require('serialport')
+var port = new SerialPort('/dev/ttyS16', { baudRate: 2000000 })
 
-var port = new SerialPort('/dev/ttyS16', { baudRate: 2000000 });
-
-port.on('data', function(data)
-{
-    // get buffered data and parse it to an utf-8 string
-    data = data.toString('utf-8');
-    console.log(data)
-    // you could for example, send this data now to the the client via socket.io
-});
-
+port.on('data', dataLink.read);
 port.on('error', function(data)
 {
-    console.log('Error: ' + data);
+    console.log('Error: ' + data)
 });
