29e28c0e88345b628daeedd5619cd2639e69c72a
Raymond <loki_fosil@hotmail.com>
2020-08-17 19:43:03 -0400

    Fix mongo string bug

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,8 @@
 module.exports = {
     db: {
-        uri: 'mongodb+srv://solargators:Q45N7sB3nGz2pnYe@tokyo-uyvve.mongodb.net/SolarGators?retryWrites=true&w=majority'
+        uri: 'mongodb://solargators:oyawhkmDlRSkUriU@tokyo-shard-00-00-uyvve.mongodb.net:27017,tokyo-shard-00-01-uyvve.mongodb.net:27017,tokyo-shard-00-02-uyvve.mongodb.net:27017/<dbname>?ssl=true&replicaSet=Tokyo-shard-0&authSource=admin&retryWrites=true&w=majority',
+
+        //uri: 'mongodb+srv://solargators:oyawhkmDlRSkUriU@tokyo-uyvve.mongodb.net/SolarGators?retryWrites=true&w=majority'
     },
     port: 8080
 };
-  
