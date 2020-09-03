7659d10f27ad96a796c38c425aa3b0532cdaf5a4
Zoe Frongillo <z.frongillo@ufl.edu>
2020-09-03 19:27:29 -0400

    BMS Unit test, checks if POST 200 was sent

---
+++ backend/test/req_test.js
@@ -0,0 +1,21 @@
+/*
+//Test the submit
+const axios = require('axios')
+
+//an example req_test for bms
+test_number = 18
+
+test_post ={
+    packSumVoltage  : test_number,
+    lowCellVoltage  : test_number,
+    highCellVoltage : test_number,
+    avgCellVoltage  : test_number
+}
+
+try {
+    axios.post('/api/bms', test_post);
+    console.log("POST to BMS submitted");
+} catch (err) {
+// TODO: do something
+}
+*/
---
+++ backend/test/server.test.js
@@ -0,0 +1,63 @@
+//Unit Test file; runs on dev mode Mocha framework
+
+//import request from 'request';
+
+// Unit Test for a POST to BMS
+/*
+describe('POST /api/bms', function () {
+
+    it('should respond with 200 status on post', function(done) {
+        request(app)
+            //an example req_test for bms
+            test_number = 18
+
+            test_post ={
+                packSumVoltage  : test_number,
+                lowCellVoltage  : test_number,
+                highCellVoltage : test_number,
+                avgCellVoltage  : test_number
+            }
+
+            //Post a test_post to the location
+            .post('/api/bms', test_post)
+
+            //Unit test expectation
+            .expect(200)
+            //expand on the "expect"(s) for the 4 parameters; make sure they are each numbers
+
+            .end(function(err, res) {
+            if (err) done(err);
+
+                });
+            done();
+        });
+});
+
+
+
+//Example request handler test
+describe('Server responds to requests', () => {
+
+    it('should respond to POST with a 200 code', (done) => {
+      request(app)
+        //an example req_test for bms
+        test_number = 18
+
+        test_post ={
+            packSumVoltage  : test_number,
+            lowCellVoltage  : test_number,
+            highCellVoltage : test_number,
+            avgCellVoltage  : test_number
+        }
+
+        //Post
+        .post('/api/bms', test_post)
+
+        request.get('http://localhost:3000', (error, response, body) => {
+          should.not.exist(error);
+          should.exist(response);
+          done();
+        });
+    });
+});
+*/
