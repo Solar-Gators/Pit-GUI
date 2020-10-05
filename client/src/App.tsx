3f0291e2bb9038dd5a16626ae3ddafd6a56b0bc5
Raymond <loki_fosil@hotmail.com>
2020-10-05 19:33:28 -0400

    Update app.tsx to use history

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Sidebar from './Sidebar'
 import LiveTelemtry from './LiveTelemetry'
+import History from './History'
 import Container from "react-bootstrap/Container"
 import Col from "react-bootstrap/Col"
 import Row from "react-bootstrap/Row"
@@ -23,6 +24,7 @@ class App extends Component
                         <Col  xs={10} id="page-content-wrapper" style = {{ padding : "30px" }}>
                             <Route exact path="/" component={LiveTelemtry} />
                             <Route exact path="/analysis" />
+                            <Route exact path="/history" component={History}/>
                         </Col> 
                     </Row>
 
